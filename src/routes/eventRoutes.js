6549c6277d11223d5130aae3c02accaca8b5acd4
unknown <singhchauhangautam@gmail.com>
2024-04-29 09:54:59 +0530

    ads-some-file-for-base

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -7,7 +7,7 @@ const eventController = require('../controller/EventsController');
 // Multer middleware configuration for handling file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log(req.file); // giving no such file in this directory
+    // console.log(req.file); // giving no such file in this directory
     const uploadDir = './uploads/';
     // console.log('Upload directory path:', uploadDir);
     // Check if the uploads directory exists
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const sanitizedDate = new Date().toISOString().replace(/:/g, '_'); // Replace colons with underscores
-    console.log (sanitizedDate)
+    // console.log (sanitizedDate)
 cb(null, sanitizedDate + '-' + file.originalname); // Naming uploaded files
 
     // cb(null, new Date().toISOString() + '-' + file.originalname); // Naming uploaded files
@@ -28,7 +28,7 @@ cb(null, sanitizedDate + '-' + file.originalname); // Naming uploaded files
 });
 const upload = multer({ storage: storage });
 
-console.log('upload', upload);
+// console.log('upload', upload);
 
 // Routes
 router.post('/event_create', upload.single('image'), (req, res, next) => {
